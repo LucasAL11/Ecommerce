a87be23b61ef617caae8d3511c0edf9ed3168a86
LucasAL11 <lucasluna1000@gmail.com>
2022-05-16 19:42:18 -0300

    Atualização Models Product

--- products/models.py
+++ products/models.py
@@ -1,77 +1,214 @@
+from os import F_OK
+from unicodedata import decimal
 from django.db import models
-from django.conf import settings
-from django.db.models.fields import CharField, SlugField
 from django.urls import reverse
-from PIL import Image
+from django.utils.translation import  gettext_lazy as _
+from mptt.models import  MPTTModel,TreeForeignKey
 
+import products
 
-# Create your models here.
+class Category(MPTTModel):
+    name = models.CharField(
+        verbose_name=_("Category name"),
+        help_text=_("Required and unique"),
+        max_length=255,
+        unique=True,
+    )
 
+    slug = models.SlugField(
+        verbose_name=_("Category safe URL"),
+        max_length=255,
+        unique=True,
+        )
 
-class ActiveProductFitler(models.Manager):
-    def get_queryset(self):
-        return super(ActiveProductFitler, self).get_queryset().filter(is_active = True)
+    parent = TreeForeignKey(
+        "self",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+        )
+    is_active = models.BooleanField(default=True)
 
-class Categories(models.Model):
-    category = models.CharField(max_length=50,db_index=True)
-    slug = models.SlugField(max_length=50, unique=True)
+    class MPTTMeta:
+        order_insertion_by = ['name']
+
+    class Meta:
+        verbose_name = _('Categoty')
+        verbose_name_plural = _('Categories')
 
     def get_absolute_url(self):
-        return reverse('products:per_category', args=[self.slug])
-
-    class Meta:
-        verbose_name = 'Categoria'
-        verbose_name_plural = 'Categorias'
-
-    def __str__(self):
-        return self.category
-
-
-class Brands(models.Model):
-    brand = models.CharField(max_length=50)
-
-    class Meta:
-        verbose_name = "Marca"
-        verbose_name_plural = 'Marcas'
-
-    def __str__(self):
-        return self.brand
-
-class Distributors(models.Model):
-    distributor = models.CharField(max_length=50,null=True)
-    
-    class Meta:
-        verbose_name = 'Distribuidora'
-        verbose_name_plural = 'Distribuidoras'
+        return reverse('products:per_category',args=[self.slug])
     
     def __str__(self):
-        return self.distributor
+        return self.name
 
+    
+
+class ProductType(models.Model):
+    name = models.CharField(
+        verbose_name= _('Product type'),
+        help_text= _('Required'),
+        max_length=255,
+        unique=True
+        )
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = _('Product type')
+        verbose_name_plural = _('Products types')
+    
+    def __str__(self):
+        return self.name
+
+class ProductSpec(models.Model):
+
+    product_type = models.ForeignKey(ProductType, on_delete=models.RESTRICT)
+    name = models.CharField(
+        verbose_name=_('Name'),
+        help_text=_('Required'),
+        max_length=255
+        )
+
+    verbose_name = _('product specificantion')
+    verbose_name_plural = _('products specifications')
+
+    def __str__(self):
+        return self.name    
 
 class Products(models.Model):
-    category = models.ForeignKey(Categories,related_name='produto', on_delete=models.CASCADE)
-    product = models.CharField(max_length=50)
-    distributor = models.ForeignKey(Distributors,on_delete=models.CASCADE)
-    description = models.CharField(max_length=50)
-    brand=  models.ForeignKey(Brands,on_delete=models.CASCADE)
-    sku =   models.CharField(max_length=50)
-    image = models.ImageField(upload_to='image/')
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    quantity_in_stock = models.IntegerField()
-    is_active = models.BooleanField(default=True)
-    create_date = models.DateTimeField(auto_now_add=True)
-    edited = models.DateTimeField(auto_now_add=True)
-    Slug =  models.SlugField(max_length=50)
-    products = ActiveProductFitler()
-    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='product_creator')
 
+    product_type = models.ForeignKey(ProductType, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.RESTRICT)
+    name = models.CharField(
+        verbose_name=_('Product Name'),
+        help_text=_('Required'),
+        max_length=255
+    )
+    slug = models.SlugField(max_length=255,default='')
+    description = models.TextField(
+        verbose_name=_('Description'),
+        help_text=_('not required'),
+        blank = True,
+        )
+
+    original_price = models.DecimalField(
+        verbose_name=_('original price'),
+        help_text=('maximum 9999'),
+        error_messages={
+            'max_length':_('This price must bebetween 0 and 999999.99'),
+        },
+        max_digits=9,
+        decimal_places=2
+    )
+
+    discount_price = models.DecimalField(
+        verbose_name=_('discount price'),
+        help_text=('maximum 9999'),
+        error_messages={
+            'max_length':_('This price must bebetween 0 and 999999.99'),
+        },
+        max_digits=9,
+        decimal_places=2
+    )
+    is_active = models.BooleanField(
+        verbose_name=_('Product is active (this will show the products in website)'),
+        help_text=_('change product visibility'),
+        default=True,
+    )
+    created_at = models.DateTimeField(_("created_at"), auto_now=True ,editable=False)
+    update_at = models.DateField(_('Updated at'), auto_now=True)
 
     class Meta:
-        verbose_name_plural = 'Produtos'
-        ordering = ('-create_date',)
-
+        
+        verbose_name = _('Product')
+        verbose_name_plural = _('Products')
+        
     def get_absolute_url(self):
-        return reverse('products:product_details', args=[self.Slug])
+        return reverse('products:product_details', args=[self.slug])
+    
+class PdtSpecValue(models.Model):
 
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+    specification = models.ForeignKey(ProductSpec, on_delete=models.CASCADE)
+    value = models.CharField(
+        verbose_name=_('value'),
+        help_text=_('Product specification value(maximum of 255 woords)'),
+        max_length=255,
+    )
+
+    class Meta:
+        verbose_name = _('Product specificatin value')
+        verbose_name_plural = _('Product specification Values')
+    
     def __str__(self):
-        return self.product
+        return self.value
+
+
+class PdtImage(models.Model):
+
+    product = models.ForeignKey(Products, on_delete=models.CASCADE, related_name="Product_image")
+    image = models.ImageField(
+        verbose_name=_('image'),
+        help_text=_('Upload a product image'),
+        upload_to='images/',
+        default='images/default.png',
+    )
+    alt_text = models.CharField(
+        verbose_name=_('Alternative text'),
+        help_text=_('Please add a alternative text'),
+        max_length=255,
+        null=True,
+        blank=True,
+    )
+    is_featured = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now=True, editable=False)
+    update_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = _('Product Image')
+        verbose_name_plural=_('Prodct images')
+
+
+#estoque
+#class PdtStock(models.Model):
+#    product = models.ForeignKey(Products)
+#    quantity = models.DecimalField(max_digits=999, decimal_places=0)
+#    brand = models.Charfield()
+#    minimum_stock = 
+#    unity_cost
+#    unity_price
+
+#fornecedores
+#class Supplier(models.model):
+#   supplier = vc
+#   phone = vc
+#   product = pk
+#   mail = vc
+#   country
+#   state
+#   city
+#   province
+#   street
+#   number
+
+#entrada de financeiro
+#class inners
+#    date = date
+#    product = fk
+#    supplier = fk
+#    quantity 
+#    unity_cost
+#    total
+
+
+#class outers
+#    date = date
+#    product = fk
+#    quantity_sold
+#    in_stock
+#    unity_cost
+#    total
+
+#class stock_control
+#https://www.youtube.com/watch?v=_ebPqXfu_tY
+#https://www.youtube.com/watch?v=br2Ups-TCWQ                
